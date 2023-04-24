0126906f1565ef59e6693561c3d63ad3165fab73
BhargavKhuha <bhargav.k@prominentpixel.com>
2023-04-24 11:29:29 +0530

    Java dataType,cmdLine code

---
+++ Java/DataTypes/typecasting.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+class typecasting{
+	public static void main(String arg[]){
+		short s = 10;
+		byte b = 10;
+		int i = s+b;
+		System.out.println(i);
+		double d = 20.2020;
+		i = (int)d;
+		System.out.println(i);
+	
+	}
+}
